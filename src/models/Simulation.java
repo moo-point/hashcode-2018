cb340c44fa137b2217371ef39a72764f2c973190
Panagiotis Spyrakis <panospurakis@gmail.com>
2018-03-01 21:52:10 +0200

    write outpu files

--- src/models/Simulation.java
+++ src/models/Simulation.java
@@ -1,5 +1,8 @@
 package models;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class Simulation {
@@ -9,20 +12,38 @@ public class Simulation {
     public int bonus;
     public int steps;
 
-    public Simulation (City city, int bonus, int steps) {
+    public Simulation(City city, int bonus, int steps) {
         this.city = city;
         this.bonus = bonus;
         this.steps = steps;
     }
 
     public void run() {
-        for (int i=0; i<steps; i++) {
+        for (int i = 0; i < steps; i++) {
             for (Vehicle vehicle : city.vehicles) {
                 vehicle.step(i);
             }
         }
+        exportData();
     }
 
+    private void exportData() {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("output.out", "UTF-8");
+            for (Vehicle vehicle : this.city.vehicles) {
+                String toExport = vehicle.rides.size() + "";
+                for (int ride : vehicle.rides) {
+                    toExport = toExport+ ride;
+                }
+                writer.println(toExport);
+            }
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     private static int nextAvailableRide = 0;
 
@@ -31,7 +52,7 @@ public class Simulation {
 
         nextAvailableRide++;
 
-        return ret;1
+        return ret;
     }
 
 
