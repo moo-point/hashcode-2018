f6564b636391d018fb893f0f3ca237bb7657159c
Nikos Linakis <nlinakis@gmail.com>
2018-03-02 08:57:14 +0200

    Removed try catch

--- src/Main.java
+++ src/Main.java
@@ -9,59 +9,55 @@ public class Main {
 
     private static Simulation simulation;
 
-    private static String filename = "e_high_bonus";
+    private static String filename = "a_example";
 
-    public static void main(String[] args) {
-        int rows=0;
-        int columns=0 ;
-        int vehicles=0;
-        int rides=0;
-        int bonus=0;
-        int steps=0;
+    public static void main(String[] args) throws Exception {
+        int rows = 0;
+        int columns = 0;
+        int vehicles = 0;
+        int rides = 0;
+        int bonus = 0;
+        int steps = 0;
 
-        try{
-            BufferedReader br = new BufferedReader(new FileReader("problem_statement/" + filename + ".in"));
-            String line;
-            int counter = 0;
-            if ((line = br.readLine()) != null) {
-                rows = Integer.valueOf(line.split(" ")[0]);
-                columns = Integer.valueOf(line.split(" ")[1]);
-                vehicles = Integer.valueOf(line.split(" ")[2]);
-                rides = Integer.valueOf(line.split(" ")[3]);
-                bonus = Integer.valueOf(line.split(" ")[4]);
-                steps = Integer.valueOf(line.split(" ")[5]);
-            }
-            System.out.println("City has " + rows + " rows and " + columns + " columns" +vehicles+ " vehicles" +rides+ " rides" +bonus+ " bonus" +steps+ " steps");
-
-
-            City.Size size = new City.Size(rows,columns);
-            ArrayList<Vehicle> vehiclesArrayList = Vehicle.factory(vehicles);
-            ArrayList<Ride> ridesArrayList = new ArrayList<>();
-            while ((line = br.readLine()) != null){
-                int rideFromRow = line.charAt(0);
-                int rideFromColumn = line.charAt(1);
-                int rideToRow = line.charAt(2);
-                int rideToColumn = line.charAt(3);
-                if (rideFromRow >=0 && rideFromColumn >=0 && rideToColumn >=0 && rideToRow >=0 &&
-                        rideFromColumn < columns && rideToColumn < columns &&
-                        rideFromRow < rows && rideToRow < rows) {
-                    Position start = new Position(rideFromRow, rideFromColumn);
-                    Position end = new Position(rideToRow, rideToColumn);
-                    Ride ride = new Ride(start, end, line.charAt(4), line.charAt(5));
-                    ridesArrayList.add(ride);
-                }
-            }
-
-            City city = new City(size,vehiclesArrayList);
-            simulation = new Simulation(city, ridesArrayList, bonus, steps);
-
-            simulation.run();
-
-            exportData();
-
-        }catch (Exception e){
-            System.out.println(e);
+        BufferedReader br = new BufferedReader(new FileReader("problem_statement/" + filename + ".in"));
+        String line;
+        int counter = 0;
+        if ((line = br.readLine()) != null) {
+            rows = Integer.valueOf(line.split(" ")[0]);
+            columns = Integer.valueOf(line.split(" ")[1]);
+            vehicles = Integer.valueOf(line.split(" ")[2]);
+            rides = Integer.valueOf(line.split(" ")[3]);
+            bonus = Integer.valueOf(line.split(" ")[4]);
+            steps = Integer.valueOf(line.split(" ")[5]);
         }
+        System.out.println("City has " + rows + " rows and " + columns + " columns" + vehicles + " vehicles" + rides + " rides" + bonus + " bonus" + steps + " steps");
+
+
+        City.Size size = new City.Size(rows, columns);
+        ArrayList<Vehicle> vehiclesArrayList = Vehicle.factory(vehicles);
+        ArrayList<Ride> ridesArrayList = new ArrayList<>();
+        while ((line = br.readLine()) != null) {
+            int rideFromRow = line.charAt(0);
+            int rideFromColumn = line.charAt(1);
+            int rideToRow = line.charAt(2);
+            int rideToColumn = line.charAt(3);
+            if (rideFromRow >= 0 && rideFromColumn >= 0 && rideToColumn >= 0 && rideToRow >= 0 &&
+                    rideFromColumn < columns && rideToColumn < columns &&
+                    rideFromRow < rows && rideToRow < rows) {
+                Position start = new Position(rideFromRow, rideFromColumn);
+                Position end = new Position(rideToRow, rideToColumn);
+                Ride ride = new Ride(start, end, line.charAt(4), line.charAt(5));
+                ridesArrayList.add(ride);
+            }
+        }
+
+        City city = new City(size, vehiclesArrayList);
+        simulation = new Simulation(city, ridesArrayList, bonus, steps);
+
+        simulation.run();
+
+        exportData();
+
     }
 
     private static void exportData() {
@@ -79,6 +75,5 @@ public class Main {
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
             System.out.println("Error occurred in Simulation. Error stacktrace: " + e.getStackTrace());
         }
-
     }
 }
